9b7b6b4a948f1c4bb5a17d7923c04e0153214fd8
Kaleab <go4ykal@gmail.com>
2021-06-21 20:19:08 -0500

    thread blocking usage of fib

--- day-01/app.js
+++ day-01/app.js
@@ -1,3 +1,6 @@
 const computations = require("./computations");
 
+console.log("1: Computing fib(25) ...");
 console.log(computations.fib(25));
+console.log("2: Computing fib(-17) ...");
+console.log(computations.fib(-17));
