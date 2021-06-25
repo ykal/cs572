8593553695a715c4bf1f6bc1497d2168c4896473
Kaleab <go4ykal@gmail.com>
2021-06-24 19:54:44 -0500

    implement endpoint to get a single course of a student

--- school/api/controllers/course.js
+++ school/api/controllers/course.js
@@ -18,7 +18,32 @@ const getStudentCourses = (req, res) => {
   })
 }
 
+const getStudentCourseById = (req, res) => {
+  const id = req && req.params && req.params.id || null;
+  const courseId = req && req.params && req.params.courseId || null;
+
+  if (!id)
+    res.status(400).send({ error: "Invalid id param" });
+  if (!courseId)
+    res.status(400).send({ error: "Invalid courseId param" });
+
+  Student.findById(id).select("courses").exec((err, student) => {
+    if (err)
+      res.status(500).send({ error: err });
+
+    if (student) {
+      const course = student.courses.id(courseId);
+      course ? res.status(200).send(course)
+        : res.status(404).send(
+          { error: `Can not find course with id '${courseId}' from student ${id}` }
+        );
+    }
+    else
+      res.status(404).send({ error: `Student with id of '${id}' not found.` })
+  })
+}
 
 module.exports = {
   getStudentCourses,
+  getStudentCourseById
 };
--- school/api/router/student.js
+++ school/api/router/student.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/", studentController.getAll);
 router.get("/:id", studentController.getById);
 router.get("/:id/courses", courseController.getStudentCourses);
+router.get("/:id/courses/:courseId", courseController.getStudentCourseById);
 
 module.exports = router;
