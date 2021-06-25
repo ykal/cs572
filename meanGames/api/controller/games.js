be6d11542b0f2c896a34a5a5cfc4561f6f31eebe
Kaleab <go4ykal@gmail.com>
2021-06-25 12:24:10 -0500

    remove unsed function

--- meanGames/api/controller/games.js
+++ meanGames/api/controller/games.js
@@ -5,14 +5,6 @@ const { Game } = require("../models");
 const MAX_DISTANCE = 100000;
 const MIN_DISTANCE = 0;
 
-const getLimit = (limitParam) => {
-  if (limitParam === null)
-    return 4
-  if (limitParam !== null && limitParam > 0 && limitParam < 8)
-    return limitParam
-  return 8;
-}
-
 const getAll = (req, res) => {
   let count = req && req.query && parseInt(req.query.count) || 5;
   let offset = req && req.query && parseInt(req.query.offset) || 0;
