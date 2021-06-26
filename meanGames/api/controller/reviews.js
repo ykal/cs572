c5f6fbbf09833789e02d8c692f020ee7cb1aae51
Kaleab <go4ykal@gmail.com>
2021-06-25 21:12:40 -0500

    implement indpoint to delete game review

--- meanGames/api/controller/reviews.js
+++ meanGames/api/controller/reviews.js
@@ -1,6 +1,16 @@
 const { Game } = require("../models");
 const { Types } = require("mongoose");
 
+const updateGame = (game, res) => {
+  game.save()
+    .then(savedGame => {
+      res.status(204).send();
+    })
+    .catch(err => {
+      res.status(500).send({ error: err });
+    });
+}
+
 const addReview = (req, res) => {
   const { id } = req.params;
   const { name, review } = req.body;
@@ -55,10 +65,35 @@ const getReview = (req, res) => {
     });
 }
 
+const deleteReview = (req, res) => {
+  const { id, reviewId } = req.params;
+  Game.findById(id)
+    .select("reviews")
+    .exec()
+    .then(game => {
+      if (!game)
+        res.status(404).send({ error: `Can not find game with id ${id}` });
+      else {
+        const review = game.reviews.id(reviewId);
+
+        if (review) {
+          console.log("here");
+          game.reviews = game.reviews.filter(item => item._id != review._id);
+          updateGame(game, res);
+        }
+        else
+          res.status(404).send({ error: `Can not find review with id ${reviewId}` });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ error: err, from: 'main' });
+    });
+}
 
 
 module.exports = {
   addReview,
   getReviews,
-  getReview
+  getReview,
+  deleteReview
 };
