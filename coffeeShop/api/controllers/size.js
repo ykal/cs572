c61e89c4bacac3e65e083d3c4ef92f64376197a4
Kaleab <go4ykal@gmail.com>
2021-06-27 16:56:45 -0500

    implement endpoint to update size for a coffee

--- coffeeShop/api/controllers/size.js
+++ coffeeShop/api/controllers/size.js
@@ -60,6 +60,52 @@ const findAll = (req, res, next) => {
 const removeById = (req, res, next) => {
   const { coffeeId, sizeId } = req.params;
 
+  const { sizeCode } = req.body;
+
+  if (!sizeCode)
+    res.status(400).send({ error: "sizeCode is required." });
+
+  Coffee.findById(coffeeId)
+    .select("sizes")
+    .exec()
+    .then(coffee => {
+      if (!coffee)
+        return Promise.reject({ appError: true, status: 404, message: `Coffee with id : ${coffeeId} not found` });
+      else {
+        const size = coffee.sizes.find(item => item._id == sizeId);
+        if (!size) {
+          return Promise.reject({ appError: true, status: 404, message: `Size with id : ${sizeId} not found` });
+        } else {
+          coffee.sizes = [
+            ...(
+              coffee.sizes ?
+                coffee.sizes.filter(item => item._id != sizeId) :
+                []
+            ),
+            {
+              ...size,
+              sizeCode
+            }
+          ];
+          return coffee.save();
+        }
+      }
+    })
+    .then(updatedCoffee => {
+      res.status(204).send();
+    })
+    .catch(error => {
+      if (error.appError)
+        res.status(error.status).send({ error: error.message });
+      else
+        next(error);
+    });
+}
+
+const updateById = (req, res, next) => {
+  const { coffeeId, sizeId } = req.params;
+  const { sizeCode } = req.body;
+
   Coffee.findById(coffeeId)
     .select("sizes")
     .exec()
@@ -92,5 +138,6 @@ const removeById = (req, res, next) => {
 module.exports = {
   add,
   findAll,
-  removeById
+  removeById,
+  updateById
 };
