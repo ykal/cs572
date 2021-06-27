178a2f3bb4b3109f097388e28551d2bb3076fd1f
Kaleab <go4ykal@gmail.com>
2021-06-27 11:27:22 -0500

    implement endpoint to find coffee by id

--- coffeeShop/api/controllers/coffee.js
+++ coffeeShop/api/controllers/coffee.js
@@ -27,7 +27,23 @@ const findAll = (req, res, next) => {
     });
 };
 
+const findOneById = (req, res, next) => {
+  const coffeeId = req.params.coffeeId;
+  Coffee.findById(coffeeId)
+    .exec()
+    .then(coffee => {
+      if (!coffee)
+        res.status(404).send({ error: `Coffee with id : ${coffeeId} not found` });
+      else
+        res.status(200).send(coffee);
+    })
+    .catch(error => {
+      next(error);
+    });
+};
+
 module.exports = {
   create,
-  findAll
+  findAll,
+  findOneById
 };
--- coffeeShop/api/router/coffee.js
+++ coffeeShop/api/router/coffee.js
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post("/", coffeeController.create);
 router.get("/", coffeeController.findAll);
+router.get("/:coffeeId", coffeeController.findOneById);
 
 module.exports = router;
