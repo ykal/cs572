6a7ec81c4aebc71761ae0f6885d5dcc6c7c29491
Kaleab <go4ykal@gmail.com>
2021-06-30 20:21:52 -0500

    add angular component to delete and add coffee

--- coffeeShop/public/angular-app/coffees/coffees.js
+++ coffeeShop/public/angular-app/coffees/coffees.js
@@ -9,4 +9,22 @@ function CoffeesController($scope, $location, CoffeeDataFactory) {
   $scope.goToCoffeeDetail = function (coffeeId) {
     $location.path(`/coffees/${coffeeId}`);
   }
+
+  $scope.onAddCoffee = () => {
+    CoffeeDataFactory.create($scope.coffee)
+      .then(coffee => {
+        $scope.coffees.push(coffee);
+        $scope.coffee = {};
+        $scope.coffeeForm.$setPristine();
+      });
+  }
+
+  $scope.onDeleteCoffee = (coffeeId) => {
+    CoffeeDataFactory.removeById(coffeeId)
+      .then(res => {
+        $scope.coffees = [
+          ...($scope.coffees.filter(coffee => coffee._id !== coffeeId))
+        ];
+      });
+  }
 }
--- coffeeShop/public/angular-app/factory/coffee-data-factory.js
+++ coffeeShop/public/angular-app/factory/coffee-data-factory.js
@@ -7,7 +7,9 @@ angular.module("coffeeApp").factory("CoffeeDataFactory", CoffeeDataFactory);
 function CoffeeDataFactory($http) {
   return {
     getAll: () => getAll($http),
-    getOneById: (coffeeId) => getOneById($http, coffeeId)
+    getOneById: (coffeeId) => getOneById($http, coffeeId),
+    create: (coffee) => create($http, coffee),
+    removeById: (coffeeId) => removeById($http, coffeeId)
   };
 };
 
@@ -19,6 +21,19 @@ function getAll($http) {
 
 function getOneById($http, coffeeId) {
   return $http.get(`${COFFEE_API}/${coffeeId}`)
+    .then(res => res.data)
+    .catch(error => console.log(error));
+}
+
+
+function create(http, coffee) {
+  return http.post(COFFEE_API, coffee)
+    .then(res => res.data)
+    .catch(error => console.log(error));
+}
+
+function removeById(http, coffeeId) {
+  return http.delete(`${COFFEE_API}/${coffeeId}`)
     .then(res => res.data)
     .catch(error => console.log(error));
 }
