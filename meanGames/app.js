ccccc9fd14c3f4dd17d99d91a0ffca601efe83c0
Kaleab <go4ykal@gmail.com>
2021-07-14 14:45:21 -0500

    resolve CORS issue

--- meanGames/app.js
+++ meanGames/app.js
@@ -2,11 +2,15 @@
 
 const express = require("express");
 require("./api/data/db");
+const cors = require("cors");
 const path = require("path");
 const { apiRouter } = require("./api");
 const PORT = 5050;
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
