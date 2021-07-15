5bf0dcbb4e2f2724454ff8ba06f85621733f9561
Kaleab <go4ykal@gmail.com>
2021-07-15 01:53:10 -0500

    page not found

--- ngCoffeeShop/src/app/app-routing.module.ts
+++ ngCoffeeShop/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthenticatedGuard } from './shared/auth/authenticated.guard';
 
 const routes: Routes = [
-    {path: "", redirectTo: "/coffees", pathMatch: "full"},
+  {path: "", redirectTo: "/coffees", pathMatch: "full"},
   {path: "auth", loadChildren: () => import('./shared/auth/auth.module').then(m => m.AuthModule), canDeactivate: []},
   {path: "coffees", loadChildren: () => import('./coffee/coffee.module').then(m => m.CoffeModule), canDeactivate: []},
-   {path: "profile", component: ProfileComponent, canActivate: [AuthenticatedGuard]}
+  {path: "profile", component: ProfileComponent, canActivate: [AuthenticatedGuard]},
+  {path: "**", component: PageNotFoundComponent}
 
 ];
 
--- ngCoffeeShop/src/app/app.module.ts
+++ ngCoffeeShop/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SharedModule } from './shared/shared.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
